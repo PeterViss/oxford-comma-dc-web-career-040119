ca889588b3c5f37edc19670825f5c6d66bcd2a78
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-03 23:03:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(' and ')
   elsif array.length === 3
-    return array.join(' , , and ')
+      array[-1].prepend "and "
+    return array.join(' , ')
   end
 end

49eab791351acfdd615a0b98f9ef34ee24d9a6eb
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-03 22:47:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,7 @@
 def oxford_comma(array)
-
-   case array.length
-end 	when 1
-  "#{array[0]}"
-when 2
-  array[0..1].join(" and ")
-else
-  array[0...-1].join(", ") << ", and #{array[-1]}"
+  array = []
+  if array == 1
+    return array.split
+  end
 end
-end  
+  

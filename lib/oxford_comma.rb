837912dd963e96fed7a44ae6f0adb7af3fe78bc6
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-03 22:28:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   string = ""
   i = 0
-  if array.length == 1 
+  if array.length == 1
     return array[0]
-  elsif array.length == 2 
+  elsif array.length == 2
     return "#{array[0]} and #{array[1]}"
-  else 
+  else
     until i == array.length - 1
       string << "#{array[i]}, "
       i += 1
@@ -14,4 +14,3 @@ def oxford_comma(array)
     return string
   end
 end
-
